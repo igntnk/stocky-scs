0578d15080e0ea71399f11b59cdfda39e3e524af
Ilya Ignatenko <ighnatenko@vk.com>
2025-06-03 10:31:42 +0300

    init version

--- setup/setup.go
+++ setup/setup.go
@@ -1,15 +1,11 @@
 package setup
 
 import (
-	"context"
 	grpcapp "github.com/igntnk/stocky-scs/grpc"
-	"github.com/igntnk/stocky-scs/repository"
 	mongorepo "github.com/igntnk/stocky-scs/repository/mongo"
 	"github.com/igntnk/stocky-scs/service"
 	"github.com/rs/zerolog"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
@@ -17,20 +13,6 @@ var grpcServer *grpc.Server
 
 func GRPCServer() *grpc.Server {
 	return grpcServer
-}
-
-func SetupDefaultData(ctx context.Context, db *mongo.Database) error {
-	userRepo := db.Collection(repository.UserCollection)
-
-	_, err := userRepo.Indexes().CreateOne(ctx, mongo.IndexModel{
-		Keys:    bson.D{{Key: " name", Value: 1}},
-		Options: options.Index().SetUnique(true),
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func Init(db *mongo.Database, isReplicaSet bool, logger zerolog.Logger) error {
